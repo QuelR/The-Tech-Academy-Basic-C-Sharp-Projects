fe3c5e3d24547785d14cb90b8a23546908419f10
QuelR <46111737+QuelR@users.noreply.github.com>
2020-06-16 19:04:50 -0600

    Add files via upload

---
+++ Methods/Methods/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Maths
+    {
+        public void Multi(int multiNum)
+        {
+            Console.WriteLine(multiNum * 10);
+        }
+
+        public void Adding(int addNum)
+        {
+            Console.WriteLine(addNum + 100);
+            
+        }
+
+        public void Divide(double divNum)
+        {
+            Console.WriteLine(divNum / 2);
+
+        }
+
+    }
+}
