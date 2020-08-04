efc4bbb48fee98068cbb5ad9d0a0c218e979ba83
QuelR <46111737+QuelR@users.noreply.github.com>
2020-08-03 21:19:44 -0600

    Page 164

---
+++ Method2/Method2/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Math
+    {
+
+        public void Adding(int addNum)
+        {
+            Console.WriteLine(addNum + 100);
+
+        }
+
+        public void Divide(decimal divNum)
+        {
+            Console.WriteLine(divNum / 2);
+
+        }
+
+        public void Third(string thirdNum)
+        {
+            int tNum = Convert.ToInt32(thirdNum);
+            int tNumTotal = tNum * 43;
+            Console.WriteLine(tNumTotal);
+
+        }
+
+    }
+}
---
+++ Method2/Method2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int number = 43;
+            string stNum = "500";
+
+            Console.WriteLine("43 plus 100 equals: ");
+            Math add = new Math();
+            add.Adding(number);
+            Console.ReadLine();
+
+            Console.WriteLine("43 divided by 2 equals: ");
+            Math divi = new Math();
+            divi.Divide(number);
+            Console.ReadLine();
+
+            Console.WriteLine("43 multiplied by 500 equals: ");
+            Math thNum = new Math();
+            thNum.Third(stNum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
