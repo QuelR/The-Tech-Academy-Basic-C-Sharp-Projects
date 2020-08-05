f9b98c697ac4c17dd80893f78eb25ba75a6c4768
QuelR <46111737+QuelR@users.noreply.github.com>
2020-08-05 16:52:38 -0600

    Page 166

---
+++ VoidMethod/VoidMethod/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Addition
+    {
+        public void Total(int numOne, int numTwo)
+        {
+            int addNum1 = numOne;
+            int addNum2 = numTwo;
+            int total = addNum1 + addNum2;
+            Console.WriteLine("Your number " + addNum1 + " plus your number " + addNum2 + " equals: " + total);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using Params;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a number");
+            string add1 = Console.ReadLine();
+            int addition1 = Int32.Parse(add1);
+
+            Console.WriteLine("Pick a second number");
+            string add2 = Console.ReadLine();
+            int addition2 = Int32.Parse(add2);
+
+            Addition addition = new Addition();
+            addition.Total(addition1, addition2);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
