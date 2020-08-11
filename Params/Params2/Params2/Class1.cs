458cfb9169d637f4e67b75b05cdf524c786869ef
QuelR <46111737+QuelR@users.noreply.github.com>
2020-08-11 13:26:25 -0600

    Redo of page 165

---
+++ Params/Params2/Params2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params2
+{
+    class Magic
+    {
+        public int Total(int numOne, string numTwo)
+        {
+            int combo;
+            if (numTwo == "")
+            {
+                combo = numOne;
+
+            }
+            else
+            {
+                int numThree = Convert.ToInt32(numTwo);
+                combo = numOne + numThree;
+            }
+
+            return combo;
+        }
+    }
+}
---
+++ Params/Params2/Params2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. 
+// Have the method do a math operation and return an integer result.
+
+//2. In the Main() method of the console app, instantiate the class.
+
+//3.Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+
+//4. Call the method in the class, passing in the one or two numbers entered.
+
+//5. Try various combinations of numbers on the code, including having no second number.
+
+namespace Params2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            string numOne = Console.ReadLine();
+            int numTwo = Int32.Parse(numOne);
+
+            Console.WriteLine("Pick a second number, if you'd like");
+            string numThree = Console.ReadLine();
+
+            
+            Magic magic = new Magic();
+            magic.Total(numTwo, numThree);
+
+            Console.WriteLine("The total equals: " + magic.Total(numTwo, numThree));
+
+            Console.ReadLine();
+
+        }
+    }
+}
