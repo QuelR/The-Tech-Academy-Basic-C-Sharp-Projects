f4208960c805a46df233b63f808a76724f1d4155
QuelR <46111737+QuelR@users.noreply.github.com>
2020-11-09 17:03:46 -0700

    Car Insurance
    Assignment on page 168.

---
+++ carInsurance/carInsurance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int ageLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            bool duiQs = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedTix = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (ageLimit > 15 && duiQs == false && speedTix <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
