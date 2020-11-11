27e5a33d086a7a348128b488015b210cf2ac4a75
QuelR <46111737+QuelR@users.noreply.github.com>
2020-11-11 12:52:03 -0700

    Resubmission
    Resubmission for the project on page 190

--- BooleanDoWhile/BooleanDoWhile/Program.cs
+++ BooleanDoWhile/BooleanDoWhile/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,34 +10,58 @@ namespace BooleanDoWhile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the current temperature?");
-            int temp = Convert.ToInt32(Console.ReadLine());
-            bool isMatch = temp == 80;
-
-            do
+            Console.WriteLine("Do you want to play my game? True or False?");
+            bool gameFun = Convert.ToBoolean(Console.ReadLine());
+            
+            while (gameFun == true)
             {
-                switch (temp)
-                {
-
-                    case 80:
-                        Console.WriteLine("It's the perfect temperature!");
-                        isMatch = true;
-                        break;
-
-                    case int n when n >= 81:
-                        Console.WriteLine("It's too warm!");
-                        isMatch = false;
-                        break;
-
-                    case int n when n <= 79:
-                        Console.WriteLine("It's too cold!");
-                        isMatch = false;
-                        break;
-                }
+                Console.ReadLine();
+                break;
             }
-            while (!isMatch && isMatch);
-            Console.ReadLine();
 
+            while (gameFun == false)
+            {
+                goto Finish;
+            }
+
+                {
+                Console.WriteLine("What is the perfect temperature?");
+                int temp = Convert.ToInt32(Console.ReadLine());
+                bool isMatch = temp == 80;
+
+                do
+                {
+                    switch (temp)
+                    {
+
+                        case 80:
+                            Console.WriteLine("That's the perfect temperature! Thanks for playing");
+                            isMatch = true;
+                            break;
+
+                        case int n when n >= 81:
+                            Console.WriteLine("That's too warm!");
+                            Console.WriteLine("\n" + "What is the perfect temperature?");
+                            temp = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case int n when n <= 79:
+                            Console.WriteLine("That's too cold!");
+                            Console.WriteLine("\n" + "What is the perfect temperature?");
+                            temp = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                }
+
+                while (!isMatch);
+
+                Console.ReadLine();
+
+
+                }
+        Finish:
+            Console.WriteLine("You're missing out. Bye.");
+            Console.ReadLine();
         }
     }
 }
