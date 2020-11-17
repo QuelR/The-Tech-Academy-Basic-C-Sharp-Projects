b9e7ad060b514247a12172651f3f3ba0be91c811
QuelR <46111737+QuelR@users.noreply.github.com>
2020-11-16 18:49:40 -0700

    ClassMethod
    Project on page 223

---
+++ ClassMethod/ClassMethod/Maths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Maths
+    {
+        public void Multi(int multiNum)
+        {
+            Console.WriteLine(multiNum * 10);
+        }
+
+        public void Adding(int addNum)
+        {
+            Console.WriteLine(addNum + 100);
+        }
+
+        public void Divide(int divNum)
+        {
+            Console.WriteLine(divNum / 2);
+        }
+    }
+}
