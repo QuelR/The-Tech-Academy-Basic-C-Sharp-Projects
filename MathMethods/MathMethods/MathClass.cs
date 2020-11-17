172ab50207a5bf0966467b429ac3a85ff15c6bf7
QuelR <46111737+QuelR@users.noreply.github.com>
2020-11-17 13:56:36 -0700

    MathMethods
    Project on page 226

---
+++ MathMethods/MathMethods/MathClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class MathClass
+    {
+        public void Adding(int addNum)
+        {
+            Console.WriteLine(addNum + 100);
+        }
+
+        public void Divide(decimal divNum)
+        {
+            Console.WriteLine(divNum / 2);
+        }
+
+        public void Third(string thirdNum)
+        {
+            int tNum = Convert.ToInt32(thirdNum);
+            int tNumTotal = tNum * 42;
+            Console.WriteLine(tNumTotal);
+        }
+    }
+}
---
+++ MathMethods/MathMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 42;
+            string stNum = "2020";
+
+            Console.WriteLine("42 plus 100 equals: ");
+            MathClass add = new MathClass();
+            add.Adding(number);
+            Console.ReadLine();
+
+            Console.WriteLine("42 divided by 2 equals: ");
+            MathClass divi = new MathClass();
+            divi.Divide(number);
+            Console.ReadLine();
+
+            Console.WriteLine("42 multiplied by 2020 equals: ");
+            MathClass thNum = new MathClass();
+            thNum.Third(stNum);
+            Console.ReadLine();
+        }
+    }
+}
