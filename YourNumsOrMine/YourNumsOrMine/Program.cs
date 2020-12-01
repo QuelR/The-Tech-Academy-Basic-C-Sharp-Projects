a4527564260a6e31dcaaee47d03d357a72fcb244
QuelR <46111737+QuelR@users.noreply.github.com>
2020-11-30 19:22:55 -0700

    Update Program.cs

--- YourNumsOrMine/YourNumsOrMine/Program.cs
+++ YourNumsOrMine/YourNumsOrMine/Program.cs
@@ -1,48 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace YourNumsOrMine
+
+namespace YourNumbersOrMine
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int number = 42;
+            Console.WriteLine("Pick a number");
+            int numOne = Convert.ToInt32(Console.ReadLine());
 
-            TwoInts twoInts = new TwoInts();
-            int userNum1 = twoInts.Num1();
-            int userNum2 = twoInts.Num2();
+            Console.WriteLine("\n" + "Pick a second number if you'd like");
+            string stringNum = Console.ReadLine();
 
-            if (userNum2 == 0)
-            {
-                Console.WriteLine("\n" + "We'll use my number: " + number);
-                int mathNum4 = number * 5;
-                Console.WriteLine("\n" + number + " x 5 is: " + mathNum4);
+            YourNumsOrMine.TwoInts twoInts = new YourNumsOrMine.TwoInts();
 
-            }
-            else if (userNum1 > userNum2)
-            {
-                Console.WriteLine("\n" + "Your first number is bigger, lets use your first number: " + userNum1);
-                int mathNum1 = userNum1 * 10;
-                Console.WriteLine("\n" + userNum1 + " x 10 is: " + mathNum1);
-            }
-
-            else if (userNum1 < userNum2 )
-            {
-                Console.WriteLine("\n" + "Your second number is bigger, lets use your second number: " + userNum2);
-                int mathNum2 = userNum2 * 15;
-                Console.WriteLine("\n" + userNum2 + " x 15 is: " + mathNum2);
-            }
-
-            else if (userNum1 == userNum2)
-            {
-                Console.WriteLine("\n" + "Your numbers are the same, lets use my number: " + number);
-                int mathNum3 = number * 20;
-                Console.WriteLine("\n" + number + " x 20 is: " + mathNum3);
-            }
+            Console.WriteLine("\n" + "The total equals: " + twoInts.Numbers(numOne, stringNum));
 
             Console.ReadLine();
         }
