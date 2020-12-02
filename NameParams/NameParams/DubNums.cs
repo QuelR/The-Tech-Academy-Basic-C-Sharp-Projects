41a7b5fc8fc8d5c7479b2c06847aa94372233936
QuelR <46111737+QuelR@users.noreply.github.com>
2020-12-01 18:09:46 -0700

    NameParams
    Project on page 228

---
+++ NameParams/NameParams/DubNums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameParams
+{
+    public class DubNums
+    {
+        public int Num1()
+        {
+            int num1 = 12;
+            return num1 * 2;
+        }
+
+        public int Num2()
+        {
+            int numTwo = 10;
+            Console.WriteLine(numTwo);
+            Console.ReadLine();
+            return numTwo;            
+        }
+    }
+}
---
+++ NameParams/NameParams/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DubNums numTwo = new DubNums();
+            int result2 = numTwo.Num2();
+
+            DubNums numOne = new DubNums();
+            int result1 = numOne.Num1();
+            Console.WriteLine("12 times 2 equals: " + result1);
+
+            Console.WriteLine("\n" + result2);
+            Console.ReadLine();
+        }
+    }
+}
