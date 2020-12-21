ecccf7d75d987de831d4a95757791487be39664e
QuelR <46111737+QuelR@users.noreply.github.com>
2020-12-21 12:44:09 -0700

    NumberLog
    Project on page 297

---
+++ NumberLog/NumberLog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//1.Asks the user for a number.
+
+//2. Logs that number to a text file.
+
+//3. Prints the text file back to the user.
+
+namespace NumberLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number.");
+            string textNum = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\raque\Logs\log.txt", true))
+            {
+                file.WriteLine(textNum);
+            }
+            Console.WriteLine("Thanks for your input of: " + textNum);
+            Console.ReadLine();
+        }
+    }
+}
